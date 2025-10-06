6ec451e6535e0ea408e81910bf7405e2da27023c
m4thk <mathiaskoefoed@gmail.com>
2025-10-06 21:35:30 +0200

    1.21.9

--- src/main/java/com/m4thk/sleeptime/SleepTime.java
+++ src/main/java/com/m4thk/sleeptime/SleepTime.java
@@ -8,6 +8,7 @@ import net.fabricmc.fabric.api.client.rendering.v1.hud.HudElementRegistry;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.option.KeyBinding.Category;
 import net.minecraft.client.util.InputUtil;
 import net.minecraft.util.Identifier;
 import net.minecraft.world.World;
@@ -15,63 +16,64 @@ import org.lwjgl.glfw.GLFW;
 
 public class SleepTime implements ClientModInitializer {
 
-	private KeyBinding toggleKey;
-	private boolean isOn = false;
-	private final Identifier LAYER = Identifier.of("sleeptime.client.layer");
+  private KeyBinding toggleKey;
+  private boolean isOn = false;
+  private final Identifier LAYER = Identifier.of("sleeptime.client.layer");
+  private final Identifier keyId = Identifier.of("key.category.com.m4thk.sleeptime");
+  private final Category KEY_CATEGORY = new Category(keyId);
 
-	@Override
-	public void onInitializeClient() {
-		MinecraftClient client = MinecraftClient.getInstance();
-		registerKey();
-		HudElementRegistry.addLast(LAYER, (drawContext, tickCounter) -> {
-			if (!isOn) {
-				return;
-			}
-			ClientPlayerEntity player = client.player;
-			if (player == null) {
-				return;
-			}
-			World world = player.getWorld();
-			if (world == null) {
-				return;
-			}
-			if (world.isThundering()) {
-				return;
-			}
-			long tick = world.isRaining() ? 12010 : 12542;
-			long get = world.getTimeOfDay() % 24000L;
-			long total = (tick - get) / 20;
-			if (total <= 0) {
-				return;
-			}
-			long min = total / 60;
-			long sec = total % 60;
-			drawContext.drawText(client.textRenderer, min + ":" + sec, 1, 1, 0xFFFFFFFF, false);
-		});
-	}
+  @Override
+  public void onInitializeClient() {
+    MinecraftClient client = MinecraftClient.getInstance();
+    registerKey();
+    HudElementRegistry.addLast(LAYER, (drawContext, tickCounter) -> {
+      if (!isOn) {
+        return;
+      }
+      ClientPlayerEntity player = client.player;
+      if (player == null) {
+        return;
+      }
+      World world = player.getEntityWorld();
+      if (world == null) {
+        return;
+      }
+      if (world.isThundering()) {
+        return;
+      }
+      long tick = world.isRaining() ? 12010 : 12542;
+      long get = world.getTimeOfDay() % 24000L;
+      long total = (tick - get) / 20;
+      if (total <= 0) {
+        return;
+      }
+      long min = total / 60;
+      long sec = total % 60;
+      drawContext.drawText(client.textRenderer, min + ":" + sec, 1, 1, 0xFFFFFFFF, false);
+    });
+  }
 
-	private void registerClientTick() {
-		ClientTickEvents.END_CLIENT_TICK.register(minecraftClient -> {
-			if (!toggleKey.wasPressed()) {
-				return;
-			}
-			if (isOn) {
-				isOn = false;
-				return;
-			}
-			isOn = true;
-		});
-	}
+  private void registerClientTick() {
+    ClientTickEvents.END_CLIENT_TICK.register(minecraftClient -> {
+      if (!toggleKey.wasPressed()) {
+        return;
+      }
+      if (isOn) {
+        isOn = false;
+        return;
+      }
+      isOn = true;
+    });
+  }
 
-	private void registerKey() {
-        String KEY_CATEGORY = "key.category.com.m4thk.sleeptime";
-        String KEY_TOGGLE = "key.com.m4thk.sleeptime.toggle";
-        toggleKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
-                KEY_TOGGLE,
-				InputUtil.Type.KEYSYM,
-				GLFW.GLFW_KEY_O,
-                KEY_CATEGORY
-		));
-		registerClientTick();
-	}
+  private void registerKey() {
+    String KEY_TOGGLE = "key.com.m4thk.sleeptime.toggle";
+    toggleKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+        KEY_TOGGLE,
+        InputUtil.Type.KEYSYM,
+        GLFW.GLFW_KEY_O,
+        KEY_CATEGORY
+    ));
+    registerClientTick();
+  }
 }
